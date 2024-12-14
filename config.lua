4c4c61463aa7e21df859877e7589a669d70b8c3e
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2024-12-14 09:22:18 +0100

    config

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Blips = {
+    {coords = vector3(441.0486, -1016.1382, 28.6878), id = 60, title = "<font face='Fire Sans'>LSPD", color = 38, scale = 0.6},
+    {coords = vector3(292.1178, -588.3205, 43.1734), id = 61, title = "<font face='Fire Sans'>EMS", color = 2, scale = 0.6},
+
+}
